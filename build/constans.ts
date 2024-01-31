948c21ea53007ee7a38535a67b4bc18e46c114e7
zzxming <zzxming77@163.com>
2024-01-31 15:46:36 +0800

    打包package.json

--- build/constans.ts
+++ build/constans.ts
@@ -3,7 +3,11 @@ import { resolve } from 'path';
 export const projRoot = resolve(__dirname, '..');
 export const pkgRoot = resolve(projRoot, 'packages');
 export const cdRoot = resolve(pkgRoot, 'cdx-component');
+export const componentRoot = resolve(pkgRoot, 'components');
 export const buildOutput = resolve(projRoot, 'dist');
 
 export const PKG_PREFIX = '@cdx-component';
 export const PKG_NAME = 'cdx-component';
+
+export const cdPackage = resolve(cdRoot, 'package.json');
+export const componentPackage = resolve(componentRoot, 'package.json');
