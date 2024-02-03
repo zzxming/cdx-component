8c06c7e37e931990f4123c2ba6771e5c615bdda9
zzxming <zzxming77@163.com>
2024-02-03 20:13:05 +0800

    test: 测试

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,26 +1,43 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import { onMounted, ref, watch } from 'vue';
-
 const v = ref([]);
 watch(v, () => {
     console.log(v.value);
 });
+
+const h = ref(false);
 </script>
 
 <template>
-    <header>
-        <div class="wrapper">youdid</div>
-    </header>
-
-    <CdElementSelect v-model="v">
-        <CdElementSelectItem
+    v-loading="true"
+    <div
+        v-loading="!h"
+        style="width: 200px; height: 200px"
+    ></div>
+    <CdxElementSelect v-model="v">
+        <CdxElementSelectItem
             v-for="info in 20"
             :trueValue="info"
         >
             <div class="item">{{ info }}</div>
-        </CdElementSelectItem>
-    </CdElementSelect>
+        </CdxElementSelectItem>
+    </CdxElementSelect>
+
+    <CdxDrawer
+        fullscreen
+        v-model="h"
+    >
+        content
+        <template #swipe>
+            <div
+                @click="h = !h"
+                style="height: 200px; border: 1px solid"
+            >
+                class
+            </div>
+        </template>
+    </CdxDrawer>
 </template>
 
 <style>
