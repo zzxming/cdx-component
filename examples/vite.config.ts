54bfa3d7b11b9ccd34c161e1b0295516d3a04f46
zzxming <zzxming77@163.com>
2024-02-03 20:13:05 +0800

    test: 测试

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Components from 'unplugin-vue-components/vite';
-import { CdComponentResolver } from 'cdx-component';
+import { CdxComponentResolver } from 'cdx-component';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -12,7 +12,7 @@ export default defineConfig({
         vueJsx(),
         Components({
             dts: false,
-            resolvers: [CdComponentResolver()],
+            resolvers: [CdxComponentResolver()],
         }),
     ],
     resolve: {
