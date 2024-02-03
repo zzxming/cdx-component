d10e6aef1dacd40705bad02eb61e5f1cda176162
zzxming <zzxming77@163.com>
2024-02-03 16:36:38 +0800

    feat: icon组件

---
+++ packages/components/icon/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '@cdx-component/utils';
+import Icon from './src/icon.vue';
+
+export { Icon };
+export const CdxIcon = withInstall(Icon);
+export default CdxIcon;
---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <i class="icon">
+        <slot></slot>
+    </i>
+</template>
