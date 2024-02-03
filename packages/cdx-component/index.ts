ab22513cde6607d5cdef99fc03c87336e38f485c
zzxming <zzxming77@163.com>
2024-02-03 18:41:02 +0800

    feat: zindex封装hook

--- packages/cdx-component/index.ts
+++ packages/cdx-component/index.ts
@@ -1,6 +1,7 @@
 export * from '../components';
 export * from '../utils';
 export * from '../constants';
+export * from '../hooks';
 
 import makeInstall from './makeInstall';
 export const install = makeInstall.install;
