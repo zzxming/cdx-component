68c39f5fcf358308f5b25c4b61f16baf22a84853
zzxming <zzxming77@163.com>
2024-02-05 15:22:51 +0800

    fix: 函数loading

--- packages/components/loading/src/loading.vue
+++ packages/components/loading/src/loading.vue
@@ -1,29 +1,46 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
 import { loadingProps } from './loading';
 
 const props = defineProps(loadingProps);
+const currentVisible = ref(props.visible);
+
+watch(
+    () => props.visible,
+    () => {
+        currentVisible.value = props.visible;
+    }
+);
 </script>
 
 <template>
-    <div :class="['loading-mask', fullscreen && 'is-fullscreen']">
-        <div class="loading-tip">
-            <svg
-                class="loading-spinner"
-                viewBox="0 0 50 50"
-            >
-                <circle
-                    cx="25"
-                    cy="25"
-                    r="20"
-                    fill="none"
-                ></circle>
-            </svg>
-            <span
-                v-if="text"
-                class="loading-text"
-            >
-                {{ text }}
-            </span>
+    <Transition
+        name="fade"
+        appear
+    >
+        <div
+            v-if="currentVisible"
+            :class="['loading-mask', fullscreen && 'is-fullscreen']"
+        >
+            <div class="loading-tip">
+                <svg
+                    class="loading-spinner"
+                    viewBox="0 0 50 50"
+                >
+                    <circle
+                        cx="25"
+                        cy="25"
+                        r="20"
+                        fill="none"
+                    ></circle>
+                </svg>
+                <span
+                    v-if="text"
+                    class="loading-text"
+                >
+                    {{ text }}
+                </span>
+            </div>
         </div>
-    </div>
+    </Transition>
 </template>
