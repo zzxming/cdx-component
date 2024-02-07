5c9726dea106bc940d92e4880852bd440a9b58f6
zzxming <zzxming77@163.com>
2024-02-07 11:38:14 +0800

    build: 导入导出

--- packages/components/text-ellipsis/index.ts
+++ packages/components/text-ellipsis/index.ts
@@ -2,4 +2,5 @@ import { withInstall } from '@cdx-component/utils';
 import TextEllipsis from './src/text-ellipsis.vue';
 
 export const CdxTextEllipsis = withInstall(TextEllipsis);
+export default CdxTextEllipsis;
 export * from './src/text-ellipsis';
