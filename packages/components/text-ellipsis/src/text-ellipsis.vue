2774eb850138c20826388618e6629a775e507d6e
zzxming <zzxming77@163.com>
2024-02-07 09:58:27 +0800

    feat: slot设置内容展开收起

--- packages/components/text-ellipsis/src/text-ellipsis.vue
+++ packages/components/text-ellipsis/src/text-ellipsis.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { computed, ref, onMounted, onBeforeUnmount, watch } from 'vue';
+import { computed, ref, onMounted, onBeforeUnmount, watch, useSlots } from 'vue';
 import { textEllipsisEmits, textEllipsisProps } from './text-ellipsis';
-import { UPDATE_MODEL_EVENT } from '@cdx-component/constants';
+import { UPDATE_MODEL_EVENT, namespace } from '@cdx-component/constants';
 import { useBem } from '@cdx-component/hooks';
 
 const props = defineProps(textEllipsisProps);
 const emits = defineEmits(textEllipsisEmits);
+const slots = useSlots();
 
 const [, bem] = useBem('text-ellipsis');
 
-const isEllipsis = ref(false);
+const isEllipsis = ref(!!slots.default);
 const isExpanded = ref(false);
 const text = ref('');
 const rootRef = ref<HTMLDivElement>();
@@ -25,15 +26,16 @@ const model = computed({
 });
 const hasExpandBtn = computed(() => props.canExpand && isEllipsis.value);
 const expandBtnText = computed(() => (model.value ? props.collapseText : props.expandText));
+const ellipsisLines = computed(() => (model.value ? 0 : props.lines));
 
-const cloneNode = <T extends HTMLElement>(node: T) => {
+const cloneNode = <T extends HTMLElement>(node: T, content: string) => {
     const copy = node.cloneNode(false) as T;
     copy.style.position = 'fixed';
     copy.style.zIndex = '-9999';
     copy.style.top = '-9999px';
     copy.style.left = '-9999px';
     copy.style.minHeight = 'auto';
-    copy.innerHTML = props.content;
+    copy.innerHTML = content;
     document.body.appendChild(copy);
     return copy;
 };
@@ -41,16 +43,16 @@ const parseNum = (num: any) => {
     const n = parseFloat(num);
     return isNaN(n) ? 0 : n;
 };
-const calcEllipsisText = (root: HTMLElement, totalHeight: number) => {
-    const len = props.content.length;
+const calcEllipsisText = (root: HTMLElement, totalHeight: number, content: string) => {
+    const len = content.length;
     // span 和文件间有换行, 不加这个空格可以把文字和 span 间的换行去掉
     const tailText = props.ellipsisText + ' ' + (hasExpandBtn.value ? expandBtnText.value : '');
     const calcDisplay = (left: number, right: number): string => {
         if (right - left <= 1) {
-            return props.content.slice(0, left) + props.ellipsisText;
+            return content.slice(0, left) + props.ellipsisText;
         }
         const mid = Math.round((left + right) / 2);
-        root.innerText = props.content.slice(0, mid) + tailText;
+        root.innerText = content.slice(0, mid) + tailText;
         if (root.offsetHeight > totalHeight) {
             return calcDisplay(left, mid);
         } else {
@@ -60,24 +62,30 @@ const calcEllipsisText = (root: HTMLElement, totalHeight: number) => {
     return calcDisplay(0, len);
 };
 const calcEllipsis = () => {
-    if (!rootRef.value) return;
-    const copy = cloneNode(rootRef.value);
+    if (!rootRef.value || !props.content) return;
+    const copy = cloneNode(rootRef.value, props.content);
     const { paddingTop, paddingBottom, lineHeight } = getComputedStyle(copy);
     const totalHeight =
         parseNum(paddingTop) + parseNum(paddingBottom) + parseNum(lineHeight) * (Number(props.lines) + 0.5);
+
     if (copy.offsetHeight > totalHeight) {
         isEllipsis.value = true;
-        text.value = calcEllipsisText(copy, totalHeight);
+        text.value = calcEllipsisText(copy, totalHeight, props.content);
     } else {
         isEllipsis.value = false;
         text.value = props.content;
     }
     document.body.removeChild(copy);
 };
+const toggleExpand = () => {
+    model.value = !model.value;
+};
 
 onMounted(() => {
-    calcEllipsis();
-    window.addEventListener('resize', calcEllipsis);
+    if (props.content) {
+        calcEllipsis();
+        window.addEventListener('resize', calcEllipsis);
+    }
 });
 onBeforeUnmount(() => {
     window.removeEventListener('resize', calcEllipsis);
@@ -88,18 +96,19 @@ watch(
         calcEllipsis();
     }
 );
-
-const toggleExpand = () => {
-    model.value = !model.value;
-};
 </script>
 
 <template>
     <div
         ref="rootRef"
         :class="bem.b()"
+        :style="{ [`--${namespace}-line`]: ellipsisLines }"
     >
-        {{ model ? props.content : text }}
+        <span :class="[$slots.default && bem.bem('content', 'block')]">
+            <slot :isExpanded="model">
+                {{ model ? props.content : text }}
+            </slot>
+        </span>
         <span
             v-if="hasExpandBtn"
             :class="bem.be('expand-btn')"
@@ -114,4 +123,3 @@ const toggleExpand = () => {
         </span>
     </div>
 </template>
-./text-ellipsis
