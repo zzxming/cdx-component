dd6a6a8290bb6c6f6d5553bb09f9bf7226879e6a
zzxming <zzxming77@163.com>
2024-02-14 18:15:51 +0800

    fix: modelValue同步

--- packages/hooks/use-model-value/index.ts
+++ packages/hooks/use-model-value/index.ts
@@ -1,5 +1,5 @@
 import { UPDATE_MODEL_EVENT } from '@cdx-component/constants';
-import { ref, computed, getCurrentInstance } from 'vue';
+import { ref, computed, getCurrentInstance, watch } from 'vue';
 export const useModelValue = <T>(props: Object & { modelValue: T }, defaultValue: T) => {
     const { emit } = getCurrentInstance()!;
 
@@ -14,5 +14,12 @@ export const useModelValue = <T>(props: Object & { modelValue: T }, defaultValue
             emit(UPDATE_MODEL_EVENT, value);
         },
     });
+
+    watch(
+        () => props.modelValue,
+        (val) => {
+            model.value = val;
+        }
+    );
     return { model };
 };
