ce49075ec287f65c80e50478192bddeee7db2130
zzxming <zzxming77@163.com>
2024-04-20 21:49:59 +0800

    🦄 refactor: 样式调整

--- packages/theme/tailwind.config.js
+++ packages/theme/tailwind.config.js
@@ -1,26 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./**/*.less'],
-    theme: {
-        extend: {
-            spacing: {
-                em: '1em',
-            },
-            animation: {
-                'fade-in': 'cdx-fade-in var(--cdx-transition-duration) ease-in-out',
-                'fade-out': 'cdx-fade-out var(--cdx-transition-duration) ease-in-out',
-                'zoom-in': 'cdx-zoom-in var(--cdx-transition-duration) ease-in-out',
-                'zoom-out': 'cdx-zoom-out var(--cdx-transition-duration) ease-in-out',
-                rotate: 'cdx-rotate 2s linear infinite',
-                'loading-dash': 'cdx-loading-dash 1.5s ease-in-out infinite',
-            },
-            boxShadow: {
-                wrap: '0px 0px 4px 2px rgb(0 0 0 / .2)',
-            },
-            transitionDuration: {
-                'transition-duration': 'var(--cdx-transition-duration)',
-            },
-        },
-    },
+    theme: {},
     plugins: [],
 };
