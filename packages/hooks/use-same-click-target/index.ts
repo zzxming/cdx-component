a1da8f97f59bd67d3f4da3bac0ee97ea9983af5b
zzxming <zzxming77@163.com>
2024-04-21 14:18:09 +0800

    🐞 fix: 修正 click 触发条件

--- packages/hooks/use-same-click-target/index.ts
+++ packages/hooks/use-same-click-target/index.ts
@@ -1,14 +1,18 @@
 export const useSameClickTarget = (callback: (e: MouseEvent) => void) => {
     let mouseDownTarget = false;
     let mouseUpTarget = false;
+    let mouseDownTime = 0;
+    let mousUpTime = 0;
     const onMouseDown = (e: MouseEvent) => {
         mouseDownTarget = e.currentTarget === e.target;
+        mouseDownTime = Date.now();
     };
     const onMouseUp = (e: MouseEvent) => {
         mouseUpTarget = e.currentTarget === e.target;
+        mousUpTime = Date.now();
     };
     const onClick = (e: MouseEvent) => {
-        if (mouseDownTarget && mouseUpTarget) {
+        if (mouseDownTarget && mouseUpTarget && mousUpTime - mouseDownTime < 200) {
             callback(e);
         }
         e.stopPropagation();
