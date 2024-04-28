5406a6d2afd4137842ea3173ba56515e1f7344f4
zzxming <zzxming77@163.com>
2024-04-28 16:49:36 +0800

    🦄 refactor: 拆分 build

--- build/gulpfile.ts
+++
@@ -1,8 +0,0 @@
-import { copyFile } from 'fs/promises';
-import { buildOutput, cdxPackage } from './constans';
-import path from 'path';
-import { task, series } from 'gulp';
-
-export const copyFiles = () => copyFile(cdxPackage, path.join(buildOutput, 'package.json'));
-
-task('default', series(copyFiles));
