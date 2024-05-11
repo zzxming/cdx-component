4f704095577c4ab0e73685f16c7adddacc6da151
zzxming <zzxming77@163.com>
2024-05-11 20:49:14 +0800

    ✨ feat: collapse transition

---
+++ packages/components/collapse-transition/index.ts
@@ -0,0 +1,5 @@
+import { withInstall } from '@cdx-component/utils';
+import CollapseTransition from './src/collapse-transition.vue';
+
+export const CdxCollapseTransition = withInstall(CollapseTransition);
+export default CdxCollapseTransition;
