0942622391f9f37a16e86c0dad4e6c4162a24473
zzxming <zzxming77@163.com>
2024-05-11 20:35:36 +0800

    📃 docs: 文档编写

---
+++ docs/demos/element-select/base.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const value = ref([]);
+</script>
+
+<template>
+    <CdxElementSelect
+        v-model="value"
+        :max="10"
+    >
+        <CdxElementSelectItem
+            class="item"
+            v-for="i in 40"
+            :trueValue="i"
+        >
+            {{ i }}
+        </CdxElementSelectItem>
+    </CdxElementSelect>
+</template>
+
+<style scoped>
+.item {
+    padding: 8px;
+}
+</style>
