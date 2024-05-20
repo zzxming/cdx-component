e75df3bdd544e4dec834cf32cacb9e96d291b193
zzxming <zzxming77@163.com>
2024-05-20 11:00:48 +0800

    📃 docs: 文档编写

---
+++ docs/demos/text-highlight/base.vue
@@ -0,0 +1,20 @@
+<template>
+    <CdxTextHighlight
+        :content="content"
+        :texts="texts"
+        :ignoreCase="false"
+        highlightTag="a"
+        highlightClass="link"
+    ></CdxTextHighlight>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+const content = ref(
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt non architecto nulla repellendus libero vero hic accusamus, suscipit itaque, dolorum placeat nostrum laboriosam vel sapiente impedit ipsa saepe voluptatem autem?',
+);
+const texts = computed(() => {
+    const words = content.value.split(' ');
+    return new Array(5).fill('').map(() => words[Math.floor(Math.random() * words.length)]);
+});
+</script>
---
+++ docs/demos/text-highlight/slot.vue
@@ -0,0 +1,25 @@
+<template>
+    <CdxTextHighlight
+        :content="content"
+        :texts="texts"
+        :ignoreCase="false"
+    >
+        <template #highlight="{ text }">
+            <span style="cursor: pointer; color: red">{{ text }}</span>
+        </template>
+        <template #default="{ text }">
+            <span style="color: blue">{{ text }}</span>
+        </template>
+    </CdxTextHighlight>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+const content = ref(
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt non architecto nulla repellendus libero vero hic accusamus, suscipit itaque, dolorum placeat nostrum laboriosam vel sapiente impedit ipsa saepe voluptatem autem?',
+);
+const texts = computed(() => {
+    const words = content.value.split(' ');
+    return new Array(5).fill('').map(() => words[Math.floor(Math.random() * words.length)]);
+});
+</script>
