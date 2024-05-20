91fd898b336cf8ee9b66341e6fd814ef0bfb5ee4
zzxming <zzxming77@163.com>
2024-05-20 10:12:49 +0800

    📃 docs: 文档编写

---
+++ docs/demos/model/service.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="startModel">open model</button>
+    <button @click="startFullscreenModel">open fullscreen model</button>
+    <div
+        ref="divRef"
+        style="position: relative; height: 200px"
+    ></div>
+</template>
+
+<script lang="ts" setup>
+import { h, ref } from 'vue';
+import { CdxModel } from 'cdx-component';
+
+const divRef = ref();
+
+const startModel = () => {
+    const { close } = CdxModel.service({
+        target: divRef.value,
+        body: 'content',
+        fullscreen: false,
+    });
+};
+const startFullscreenModel = () => {
+    const { close } = CdxModel.service({
+        body: 'content',
+        header: h('div', 'header'),
+        footer: h('div', 'footer'),
+    });
+};
+</script>
