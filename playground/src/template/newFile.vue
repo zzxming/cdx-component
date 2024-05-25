6484942b61883adb2e4582b658ce3894b552d62d
zzxming <zzxming77@163.com>
2024-05-25 20:18:48 +0800

    🐞 fix: playground 初始代码

---
+++ playground/src/template/newFile.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { CdxTextEllipsis } from 'cdx-component';
+</script>
+
+<template>
+    <CdxTextEllipsis
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit adipisci nulla tempora alias distinctio reiciendis soluta, aliquid nostrum et magni architecto consequatur voluptate provident harum quos! Laborum, impedit ea."
+    >
+    </CdxTextEllipsis>
+</template>
