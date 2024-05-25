da8b8d9a76e49621ad72905ec1bf4509e9e592b6
zzxming <zzxming77@163.com>
2024-05-25 20:18:48 +0800

    🐞 fix: playground 初始代码

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 import { ref, watchEffect } from 'vue';
 import Monaco from '@vue/repl/monaco-editor';
-import { Repl, useStore, useVueImportMap, File } from '@vue/repl';
+import { Repl, useStore, useVueImportMap, File, compileFile } from '@vue/repl';
 import setupCode from './template/setup.js?raw';
 import mainCode from './template/main.vue?raw';
-import appCode from './template/App.vue?raw';
+import welcomeSFCCode from './template/welcome.vue?raw';
+import newSFCCode from './template/newFile.vue?raw';
 
+const query = new URLSearchParams(location.search);
 const {
     importMap: builtinImportMap,
     vueVersion,
@@ -21,25 +23,34 @@ const mainFile = 'src/main.vue';
 const appFile = 'src/App.vue';
 
 builtinImportMap.value.imports!['cdx-component'] = '/cdx-component/index.js';
+
+const files = ref<Record<string, File>>({
+    [appFile]: new File(appFile, welcomeSFCCode, false),
+    [setupFile]: new File(setupFile, setupCode, true),
+    [mainFile]: new File(mainFile, mainCode, true),
+});
+
 const store = useStore(
     {
+        showOutput: ref(query.has('showOutput')),
         builtinImportMap,
         vueVersion,
-        files: ref({
-            [appFile]: new File(appFile, appCode, false),
-            [setupFile]: new File(setupFile, setupCode, true),
-            [mainFile]: new File(mainFile, mainCode, true),
+        files,
+        template: ref({
+            welcomeSFC: welcomeSFCCode,
+            newSFC: newSFCCode,
         }),
-        mainFile: ref(mainFile),
+        // mainFile: ref(mainFile),
         // activeFilename: ref(appFile),
     },
     location.hash,
 );
-store.files[setupFile].hidden = true;
-store.files[mainFile].hidden = true;
-store.init();
+// 在 option 中设置会导致 files 里的 compiler 是空而无法解析到 iframe 中
+for (const key in files.value) {
+    compileFile(store, files.value[key]);
+}
+store.mainFile = mainFile;
 store.setActive(appFile);
-
 console.log(store);
 console.log(store.files);
 watchEffect(() => history.replaceState({}, '', store.serialize()));
