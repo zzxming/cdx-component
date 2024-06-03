04358bc042f6730fbafa72c509e19a5ae9b7baa1
zzxming <zzxming77@163.com>
2024-06-03 09:20:05 +0800

    🔧 build: 生成全量类型

--- internal/build/vite.full.config.ts
+++ internal/build/vite.full.config.ts
@@ -1,21 +1,35 @@
-import { defineConfig, type Plugin } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { resolve } from 'path';
 import commonjs from '@rollup/plugin-commonjs';
 import esbuild from 'rollup-plugin-esbuild';
-import { buildOutput, cdxPackage, rollupExternalFromPackage, cdxRoot } from '@cdx-component/build-utils';
+import {
+    buildOutput,
+    cdxPackage,
+    rollupExternalFromPackage,
+    cdxRoot,
+    buildRoot,
+    pkgRoot,
+} from '@cdx-component/build-utils';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     plugins: [
         vue(),
         vueJsx(),
-        commonjs() as Plugin,
+        commonjs(),
         esbuild({
             target: 'chrome64',
             loaders: {
                 '.vue': 'js',
             },
+        }),
+        dts({
+            entryRoot: pkgRoot,
+            outDir: buildOutput,
+            tsconfigPath: resolve(buildRoot, 'tsconfig.json'),
+            rollupTypes: true,
         }),
     ],
     build: {
@@ -23,13 +37,7 @@ export default defineConfig({
         minify: false,
         rollupOptions: {
             input: resolve(cdxRoot, 'index.ts'),
-            external: rollupExternalFromPackage(
-                cdxPackage,
-                (id: string) => {
-                    return /css$/.test(id);
-                },
-                { full: true },
-            ),
+            external: rollupExternalFromPackage(cdxPackage, (id: string) => /css$/.test(id), { full: true }),
             treeshake: false,
             preserveEntrySignatures: 'allow-extension',
             output: [
