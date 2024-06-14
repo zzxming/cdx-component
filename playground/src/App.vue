435be172ccd9fd5f6597ea9308f5bf0e85ff6887
zzxming <zzxming77@163.com>
2024-06-14 15:59:07 +0800

    ✨ feat: 增加 playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,69 +1,37 @@
 <script lang="ts" setup>
-import { ref, watchEffect } from 'vue';
+import { ref } from 'vue';
 import Monaco from '@vue/repl/monaco-editor';
-import { Repl, useStore, useVueImportMap, File, compileFile } from '@vue/repl';
-import setupCode from './template/setup.js?raw';
-import mainCode from './template/main.vue?raw';
-import welcomeSFCCode from './template/welcome.vue?raw';
-import newSFCCode from './template/newFile.vue?raw';
+import { Repl, OutputModes } from '@vue/repl';
+import Header from './components/header.vue';
+
+const handleKeydown = (evt: KeyboardEvent) => {
+    if ((evt.ctrlKey || evt.metaKey) && evt.code === 'KeyS') {
+        evt.preventDefault();
+        return;
+    }
+};
+
+import { useReplStore } from './store';
 
 const query = new URLSearchParams(location.search);
-const {
-    importMap: builtinImportMap,
-    vueVersion,
-    productionMode,
-} = useVueImportMap({
-    vueVersion: '3.3.11',
+
+const store = useReplStore({
+    showOutput: ref(query.has('showOutput')),
+    outputMode: ref((query.get('outputMode') as OutputModes) || 'preview'),
 });
-
-productionMode.value = false;
-
-const setupFile = 'src/setup.js';
-const mainFile = 'src/main.vue';
-const appFile = 'src/App.vue';
-
-builtinImportMap.value.imports!['cdx-component'] = '/cdx-component/index.js';
-
-const files = ref<Record<string, File>>({
-    [appFile]: new File(appFile, welcomeSFCCode, false),
-    [setupFile]: new File(setupFile, setupCode, true),
-    [mainFile]: new File(mainFile, mainCode, true),
-});
-
-const store = useStore(
-    {
-        showOutput: ref(query.has('showOutput')),
-        builtinImportMap,
-        vueVersion,
-        files,
-        template: ref({
-            welcomeSFC: welcomeSFCCode,
-            newSFC: newSFCCode,
-        }),
-        // mainFile: ref(mainFile),
-        // activeFilename: ref(appFile),
-    },
-    location.hash,
-);
-// 在 option 中设置会导致 files 里的 compiler 是空而无法解析到 iframe 中
-for (const key in files.value) {
-    compileFile(store, files.value[key]);
-}
-store.mainFile = mainFile;
-store.setActive(appFile);
 console.log(store);
-console.log(store.files);
-watchEffect(() => history.replaceState({}, '', store.serialize()));
 </script>
 
 <template>
+    <Header :store="store"></Header>
     <Repl
         :store="store"
         show-compile-output
         auto-resize
         :clear-console="false"
         :editor="Monaco"
-        :theme="'light'"
+        theme="light"
+        @keydown="handleKeydown"
     ></Repl>
 </template>
 
--- playground/src/template/newFile.vue
+++ playground/src/template/newFile.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-import { CdxTextEllipsis } from 'cdx-component';
-</script>
+<script setup lang="ts"></script>
 
-<template>
-    <CdxTextEllipsis
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit adipisci nulla tempora alias distinctio reiciendis soluta, aliquid nostrum et magni architecto consequatur voluptate provident harum quos! Laborum, impedit ea."
-    >
-    </CdxTextEllipsis>
-</template>
+<template></template>
