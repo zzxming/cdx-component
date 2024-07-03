f04e18512931a68c2b17f0993928fbc6392a5397
zzxming <zzxming77@163.com>
2024-07-03 15:44:39 +0800

    fix: vue props 处理函数 validator 类型错误

--- packages/utils/vue/types.ts
+++ packages/utils/vue/types.ts
@@ -5,12 +5,12 @@ export type Value<T> = T[keyof T];
 export type Writable<T> = { -readonly [P in keyof T]: T[P] };
 export type WritableArray<T> = T extends readonly any[] ? Writable<T> : T;
 
-export interface PropOptions<Type = never, Value = never, Default = never, Required extends boolean = false> {
+export interface PropOptions<Type = never, Value = never, Validator = never, Default = never, Required extends boolean = false> {
   type?: Type;
   required?: Required;
   default?: Default;
   values?: readonly Value[];
-  validator?: (val: any) => boolean;
+  validator?: (Validator) | ((val: any) => boolean);
 }
 
 export type ExtractPropType<T extends object> = Value<
@@ -18,6 +18,7 @@ export type ExtractPropType<T extends object> = Value<
     key: T;
   }>
 >;
+
 export type ResolvePropType<T> = IfNever<
   T,
   never,
@@ -26,16 +27,23 @@ export type ResolvePropType<T> = IfNever<
     required: true;
   }>
 >;
-export type MergePropType<Type, Value> =
-  | IfNever<UnknownToNever<Value>, ResolvePropType<Type>, never>
-  | UnknownToNever<Value>;
 
-export type ConvertProps<Input> = Input extends PropOptions<infer Type, infer Value, any, infer Required>
-  ? FinalProps<Type, Value, Input['default'], Required>
+export type MergePropType<Type, Value, Validator> =
+  | IfNever<UnknownToNever<Value>, ResolvePropType<Type>, never>
+  | UnknownToNever<Value>
+  | UnknownToNever<Validator>;
+
+export type ConvertProps<Input> = Input extends PropOptions<infer Type, infer Value, infer Validator, any, infer Required>
+  ? FinalProps<Type, Value, Validator, Input['default'], Required>
   : never;
-export type FinalProps<Type, Value, Default, Required> = {
-  readonly type: PropType<MergePropType<Type, Value>>;
+
+export type FinalProps<Type, Value, Validator, Default, Required> = {
+  readonly type: PropType<MergePropType<Type, Value, Validator>>;
   readonly required: [Required] extends [true] ? true : false;
-  readonly validator: ((val: unknown) => boolean) | undefined;
+  readonly validator: IfNever<
+    IfNever<UnknownToNever<Validator>, UnknownToNever<Value>, UnknownToNever<Validator>>,
+    undefined,
+    ((val: unknown) => boolean)
+  >;
   [PropKey]: true;
 } & IfNever<UnknownToNever<Default>, unknown, { readonly default: Default }>;
