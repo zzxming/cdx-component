9c8537aeed53aa7c6046aca81b685ca8ed705070
zzxming <zzxming77@163.com>
2024-07-04 20:52:03 +0800

    refactor: 使用 eslint 格式化生成

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,8 +1,7 @@
 import { resolve } from 'node:path';
 import fs from 'fs-extra';
-import buildUtils from '@cdx-component/build-utils';
+import { componentRoot } from '@cdx-component/build-utils';
 
-const { componentRoot, projRoot } = buildUtils;
 export const components = fs.readdirSync(componentRoot).filter((f) => {
   const path = resolve(componentRoot, f);
 
@@ -12,17 +11,3 @@ export const components = fs.readdirSync(componentRoot).filter((f) => {
 
   return fs.existsSync(`${path}/index.ts`);
 });
-
-let prettierConfig: any = null;
-export const getPrettierConfig = async () => {
-  if (prettierConfig) return prettierConfig;
-  const data = fs.readFileSync(resolve(projRoot, '.prettierrc'), { encoding: 'utf8' });
-  try {
-    const config = JSON.parse(data);
-    prettierConfig = config;
-    return prettierConfig;
-  }
-  catch {
-    throw new Error('Invalid prettier config');
-  }
-};
