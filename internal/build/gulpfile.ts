75f6b2e6380a6bcb6020994e5c131c230be77dee
zzxming <zzxming77@163.com>
2024-07-05 13:53:41 +0800

    fix: 命令执行路径错误

--- internal/build/gulpfile.ts
+++
@@ -1,7 +0,0 @@
-import { copyFile } from 'node:fs/promises';
-import path from 'node:path';
-import { series, task } from 'gulp';
-import { buildOutput, cdxPackage } from '@cdx-component/build-utils';
-
-export const copyFiles = () => copyFile(cdxPackage, path.join(buildOutput, 'package.json'));
-task('default', series(copyFiles));
