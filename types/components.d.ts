588d4ec4ba6dde1babab36a9f35bacc0d9794343
zzxming <zzxming77@163.com>
2024-07-06 13:16:36 +0800

    refactor: 修改导入类型

--- types/components.d.ts
+++ types/components.d.ts
@@ -6,6 +6,10 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     CdxCaptcha: (typeof import('../packages/cdx-component'))['CdxCaptcha'];
     CdxCaptchaSlider: (typeof import('../packages/cdx-component'))['CdxCaptchaSlider'];
+    CdxCollapse: (typeof import('../packages/cdx-component'))['CdxCollapse'];
+    CdxCollapseItem: (typeof import('../packages/cdx-component'))['CdxCollapseItem'];
+    CdxCollapseTransition: (typeof import('../packages/cdx-component'))['CdxCollapseTransition'];
+    CdxCountTo: (typeof import('../packages/cdx-component'))['CdxCountTo'];
     CdxDrawer: (typeof import('../packages/cdx-component'))['CdxDrawer'];
     CdxElementSelect: (typeof import('../packages/cdx-component'))['CdxElementSelect'];
     CdxElementSelectItem: (typeof import('../packages/cdx-component'))['CdxElementSelectItem'];
@@ -13,11 +17,10 @@ declare module '@vue/runtime-core' {
     CdxLoading: (typeof import('../packages/cdx-component'))['CdxLoading'];
     CdxModel: (typeof import('../packages/cdx-component'))['CdxModel'];
     CdxOverlay: (typeof import('../packages/cdx-component'))['CdxOverlay'];
+    CdxPullRefresh: (typeof import('../packages/cdx-component'))['CdxPullRefresh'];
     CdxTextEllipsis: (typeof import('../packages/cdx-component'))['CdxTextEllipsis'];
     CdxTextHighlight: (typeof import('../packages/cdx-component'))['CdxTextHighlight'];
-    CdxCountTo: (typeof import('../packages/cdx-component'))['CdxCountTo'];
-    CdxCollapse: (typeof import('../packages/cdx-component'))['CdxCollapse'];
-    CdxCollapseTransition: (typeof import('../packages/cdx-component'))['CdxCollapseTransition'];
-    CdxCollapseItem: (typeof import('../packages/cdx-component'))['CdxCollapseItem'];
   }
 }
+
+export {};
--- types/env.d.ts
+++ types/env.d.ts
@@ -1,7 +1,9 @@
 import type { INSTALLED_KEY } from '@cdx-component/constants';
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
   export interface App {
     [INSTALLED_KEY]?: boolean;
   }
 }
+
+export {};
