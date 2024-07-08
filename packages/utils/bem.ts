f0a58aff27dd85ec029074995f85f875b480ed9a
zzxming <zzxming77@163.com>
2024-07-08 17:26:05 +0800

    refactor: bem 可生成无前缀字符串

--- packages/utils/bem.ts
+++ packages/utils/bem.ts
@@ -1,21 +1,24 @@
-export const createBEM = (n: string, b: string) => ({
-  /** n-b */
-  b: () => `${n}-${b}`,
-  /** n-b__e */
-  be: (e: string) => `${n}-${b}__${e}`,
-  /** n-b--m */
-  bm: (m: string) => `${n}-${b}--${m}`,
-  /** n-b__e--m */
-  bem: (e: string, m: string) => `${n}-${b}__${e}--${m}`,
-  /** n-s */
-  ns: (s: string) => `${n}-${s}`,
-  /** n-b-s */
-  bs: (s: string) => `${n}-${b}-${s}`,
-  /** --n-v */
-  cv: (v: string) => `--${n}-${v}`,
-});
+export const createBEM = (n: string, b: string) => {
+  const prefix = n ? `${n}-` : '';
+  return {
+    /** n-b */
+    b: () => `${prefix}${b}`,
+    /** n-b__e */
+    be: (e: string) => `${prefix}${b}__${e}`,
+    /** n-b--m */
+    bm: (m: string) => `${prefix}${b}--${m}`,
+    /** n-b__e--m */
+    bem: (e: string, m: string) => `${prefix}${b}__${e}--${m}`,
+    /** n-s */
+    ns: (s: string) => `${prefix}${s}`,
+    /** n-b-s */
+    bs: (s: string) => `${prefix}${b}-${s}`,
+    /** --n-v */
+    cv: (v: string) => `--${prefix}${v}`,
+  };
+};
 
-export const createNamespace = (namespace: string, name: string): [string, ReturnType<typeof createBEM>] => {
-  const preName = `${namespace}-${name}`;
+export const createNamespace = (name: string, namespace: string = ''): [string, ReturnType<typeof createBEM>] => {
+  const preName = `${namespace ? `${namespace}-` : ''}${name}`;
   return [preName, createBEM(namespace, name)];
 };
