a88e8cba36eabdcf4fd71fb580f4995438b7c0ba
zzxming <zzxming77@163.com>
2024-07-11 21:30:04 +0800

    test: overlay 测试

--- packages/components/overlay/src/overlay.vue
+++ packages/components/overlay/src/overlay.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, nextTick, onBeforeUnmount, onMounted, ref, watch } from 'vue';
+import { StyleValue, computed, nextTick, onBeforeUnmount, onMounted, ref, watch } from 'vue';
 import { useBem, useLockScroll, useModelValue, useSameClickTarget } from '@cdx-component/hooks';
 import { overlayEmits, overlayProps } from './overlay';
 
@@ -14,7 +14,7 @@ const { onMouseDown, onMouseUp, onClick } = useSameClickTarget(e => emits('click
 
 const overlayRef = ref<HTMLElement>();
 
-const overlayStyle = computed(() => ({
+const overlayStyle = computed<StyleValue>(() => ({
   position: props.fullscreen ? 'fixed' : 'absolute',
 }));
 
