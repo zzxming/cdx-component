e850d32e2df920517ef8d10c0238b832952ac4b8
zzxming <zzxming77@163.com>
2024-07-27 19:29:06 +0800

    test: add component test

---
+++ packages/components/overlay/__tests__/ssr.test.tsx
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest';
+import { renderToString } from '@vue/test-utils';
+import Overlay from '../src/overlay.vue';
+
+describe('SSR for Overlay', () => {
+  it('render', async () => {
+    try {
+      const html = await renderToString(<Overlay modelValue={true}>content</Overlay>);
+      expect(html).toContain('content');
+      expect(html).toContain('<div class="cdx-overlay" style="position:absolute;">');
+    }
+    catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+
+  it('render fullscreen', async () => {
+    try {
+      const html = await renderToString(<Overlay modelValue={true} fullscreen={true}>content</Overlay>);
+      expect(html).toContain('content');
+      expect(html).toContain('<div class="cdx-overlay" style="position:fixed;">');
+    }
+    catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+});
---
+++ packages/components/pull-refresh/__tests__/ssr.test.tsx
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest';
+import { renderToString } from '@vue/test-utils';
+import PullRefresh from '../src/pull-refresh.vue';
+
+describe('SSR for TextEllipsis', () => {
+  it('render', async () => {
+    try {
+      const html = await renderToString(
+        <PullRefresh>
+          {{
+            default: () => new Array(10).fill(0).map((_, i) => <li>{i}</li>),
+          }}
+        </PullRefresh>,
+      );
+      expect(html).toContain('<div class="cdx-pull-refresh">');
+      expect(html).toContain(new Array(10).fill(0).map((_, i) => `<li>${i}</li>`).join(''));
+    }
+    catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+});
