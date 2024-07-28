6a6e068f064af7377c17e1f5ad2c22836f73175f
zzxming <zzxming77@163.com>
2024-07-28 18:51:52 +0800

    fix: add playground dep version

--- docs/.vitepress/composables/use-playground.ts
+++ docs/.vitepress/composables/use-playground.ts
@@ -3,7 +3,14 @@ import { utoa } from '../utils';
 const MAIN_FILE_NAME = 'App.vue';
 
 export const usePlayground = (code: string) => {
-  const originCode = { [MAIN_FILE_NAME]: code };
+  const originCode = {
+    [MAIN_FILE_NAME]: code,
+    __versions: {
+      'cdx-component': 'latest',
+      'vue': '3.3.11',
+      'typescript': 'latest',
+    },
+  };
   const encoded = utoa(JSON.stringify(originCode));
   const link = `https://zzxming.github.io/cdx-component-playground/#${encoded}`;
   return {
