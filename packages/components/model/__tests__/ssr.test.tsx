4a98ea2fd90fbd07060f307e0042bc77ae468827
zzxming <zzxming77@163.com>
2024-07-29 21:11:23 +0800

    test: add component test

---
+++ packages/components/model/__tests__/ssr.test.tsx
@@ -0,0 +1,44 @@
+import { describe, expect, it } from 'vitest';
+import { renderToString } from '@vue/test-utils';
+import Model from '..';
+
+describe('SSR for Model', () => {
+  it('render', async () => {
+    try {
+      const html = await renderToString(
+        <Model modelValue={true}>
+          {{
+            header: () => 'header',
+            default: () => 'default',
+            footer: () => 'footer',
+          }}
+        </Model>,
+      );
+      expect(html).toContain('<div class="cdx-model__content">');
+      expect(html).toContain('<div class="cdx-model__header">');
+      expect(html).toContain('<div class="cdx-model__body">');
+      expect(html).toContain('<div class="cdx-model__footer">');
+    }
+    catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+
+  it('render hide', async () => {
+    try {
+      const html = await renderToString(
+        <Model>
+          {{
+            header: () => 'header',
+            default: () => 'default',
+            footer: () => 'footer',
+          }}
+        </Model>,
+      );
+      expect(html).not.toContain('<div class="cdx-model__content">');
+    }
+    catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+});
