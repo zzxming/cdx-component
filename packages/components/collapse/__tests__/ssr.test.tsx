9205acf5cef44bd852927f663c35c48a5c66c9ba
zzxming <zzxming77@163.com>
2024-07-30 10:55:30 +0800

    test: add component test

---
+++ packages/components/collapse/__tests__/ssr.test.tsx
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import { renderToString } from '@vue/test-utils';
+import { CdxCollapse, CdxCollapseItem } from '..';
+
+describe('SSR for Collapse', () => {
+  it('render expend', async () => {
+    try {
+      const html = await renderToString(
+        () => (
+          <CdxCollapse modelValue={['2']}>
+            <CdxCollapseItem name="1" title="1">content 1</CdxCollapseItem>
+            <CdxCollapseItem name="2" title="2">content 2</CdxCollapseItem>
+          </CdxCollapse>
+        ),
+      );
+      expect(html).not.toContain('content 1');
+      expect(html).toContain('content 2');
+    }
+    catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+});
