ab4233b3c30003fbbf828623f2da13625479f9ac
zzxming <zzxming77@163.com>
2024-08-03 05:13:40 +0800

    fix: component name registe undefined

--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -1,12 +1,10 @@
-import type { App, Directive, Plugin } from 'vue';
+import type { App, Component, Directive, Plugin } from 'vue';
 
 export type SFCWithInstall<T> = T & Plugin;
 
-export const withInstall = <T, E extends Record<string, any>>(main: T, extra?: E) => {
+export const withInstall = <T extends Component, E extends Record<string, any>>(main: T, extra?: E) => {
   (main as SFCWithInstall<T>).install = (app: App): void => {
-    for (const comp of [main, ...Object.values(extra ?? {})]) {
-      app.component(comp.name, comp);
-    }
+    app.component(main.name!, main);
   };
 
   if (extra) {
