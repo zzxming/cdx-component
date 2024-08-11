8394a2f2317a73249c3bdd2539852fa166d57b82
zzxming <zzxming77@163.com>
2024-08-11 20:19:38 +0800

    refactor: extract direction

--- packages/components/resize/src/constants.ts
+++ packages/components/resize/src/constants.ts
@@ -3,7 +3,7 @@ import type { InjectionKey, Ref } from 'vue';
 export interface ResizeInjectionContext {
   contentRef: Ref<HTMLElement | undefined>;
 };
-export type Direction = 'top' | 'right' | 'bottom' | 'left';
 
 export const RESIZE_INJECTION_KEY: InjectionKey<ResizeInjectionContext> = Symbol('resize_inject');
 export const resizeValidDirection = ['top', 'right', 'bottom', 'left'] as const;
+export type Direction = typeof resizeValidDirection[number];
