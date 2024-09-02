eeb97d981254dbb606ef5a952073c21e0c37b82f
zzxming <zzxming77@163.com>
2024-09-02 18:30:40 +0800

    feat: eslint add vitest rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,21 @@ import { factory } from '@zzxming/eslint-config';
 
 export default factory({
   tailwindcss: true,
+  vitest: {
+    files: ['**/__tests__/*.{test,spec}.?([cm])[jt]s?(x)', '**/__tests__/*.{test,spec}-d.?([cm])[jt]s?(x)'],
+  },
   overrides: [
     {
       rules: {
         'ts/no-unused-expressions': 'off',
+
+        'vitest/prefer-lowercase-title': 'off',
+      },
+    },
+    {
+      files: ['**/__tests__/*.{test,spec}-d.?([cm])[jt]s?(x)'],
+      rules: {
+        'vitest/expect-expect': 'off',
       },
     },
   ],
