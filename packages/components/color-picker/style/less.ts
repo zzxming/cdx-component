763fa0eec3c5585e2008f6dc815e5fabd9eb0c92
zzxming <zzxming77@163.com>
2024-12-05 17:31:59 +0800

    fix: less style file patch

---
+++ packages/components/color-picker/style/less.ts
@@ -0,0 +1,2 @@
+import '@cdx-component/components/base/style/less';
+import '@cdx-component/theme/src/color-picker.less';
--- packages/components/scrollbar/style/less.ts
+++ packages/components/scrollbar/style/less.ts
@@ -1,2 +1,2 @@
-import '@cdx-component/components/base/less';
+import '@cdx-component/components/base/style/less';
 import '@cdx-component/theme/scrollbar.less';
