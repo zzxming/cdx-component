8f73c9dd6ba358f64c7c257e09b7d60d6e2de711
zzxming <zzxming77@163.com>
2025-11-06 19:01:08 +0800

    feat: watermark

---
+++ packages/hooks/use-watermark/index.ts
@@ -0,0 +1,261 @@
+import type { CSSProperties, Ref, UnwrapRef } from 'vue';
+import { isArray, isUndefined } from '@cdx-component/utils';
+import { useMutationObserver } from '@vueuse/core';
+import { computed, onMounted, ref, watch } from 'vue';
+import { getClips } from './clip';
+
+export interface ContentFont {
+  color: string;
+  fontSize: number;
+  fontWeight: string;
+  fontFamily: string;
+  fontGap: number;
+  textAlign: 'start' | 'end' | 'left' | 'right' | 'center';
+  textBaseline: 'top' | 'hanging' | 'middle' | 'alphabetic' | 'ideographic' | 'bottom';
+}
+export interface WatermarkOptions {
+  content?: Ref<string | string[]>;
+  image?: Ref<string>;
+  rotate: Ref<number>;
+  gap: Ref<[number, number]>;
+  zIndex: Ref<number>;
+  offset: Ref<[number, number]>;
+  font: Partial<ContentFont>;
+}
+
+export function toLowercaseSeparator(key: string) {
+  return key.replaceAll(/([A-Z])/g, '-$1').toLowerCase();
+}
+
+export function getStyleStr(style: CSSProperties): string {
+  return Object.keys(style)
+    .map(key => `${toLowercaseSeparator(key)}: ${style[key as keyof CSSProperties]};`)
+    .join(' ');
+}
+
+export function getPixelRatio() {
+  return window.devicePixelRatio || 1;
+}
+export const useWatermark = (el: Ref<HTMLElement | undefined>, options: Partial<WatermarkOptions>) => {
+  const containerRef = ref<HTMLElement>();
+  const watermarkRef = ref<HTMLElement>();
+  const stopObservation = ref(false);
+  const content = computed(() => options.content?.value);
+  const image = computed(() => options.image?.value);
+  const rotate = computed(() => options.rotate?.value || -22);
+  const zIndex = computed(() => options.zIndex?.value || 10);
+  const gapX = computed(() => options.gap?.value[0] || 100);
+  const gapY = computed(() => options.gap?.value[1] || 100);
+  const gapXCenter = computed(() => gapX.value / 2);
+  const gapYCenter = computed(() => gapY.value / 2);
+  const offsetLeft = computed(() => options.offset?.value[0] ?? gapXCenter.value);
+  const offsetTop = computed(() => options.offset?.value[1] ?? gapYCenter.value);
+  const fontOptions = computed<ContentFont>(() => {
+    const defaultFont: ContentFont = {
+      color: 'rgba(0,0,0,.15)',
+      fontSize: 16,
+      fontWeight: 'normal',
+      fontFamily: 'sans-serif',
+      fontGap: 3,
+      textAlign: 'center',
+      textBaseline: 'alphabetic',
+    };
+    return { ...defaultFont, ...options.font };
+  });
+
+  const getMarkStyle = () => {
+    const markStyle: CSSProperties = {
+      zIndex: zIndex.value,
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: '100%',
+      height: '100%',
+      pointerEvents: 'none',
+      backgroundRepeat: 'repeat',
+    };
+
+    let positionLeft = offsetLeft.value - gapXCenter.value;
+    let positionTop = offsetTop.value - gapYCenter.value;
+    if (positionLeft > 0) {
+      markStyle.left = `${positionLeft}px`;
+      markStyle.width = `calc(100% - ${positionLeft}px)`;
+      positionLeft = 0;
+    }
+    if (positionTop > 0) {
+      markStyle.top = `${positionTop}px`;
+      markStyle.height = `calc(100% - ${positionTop}px)`;
+      positionTop = 0;
+    }
+    markStyle.backgroundPosition = `${positionLeft}px ${positionTop}px`;
+
+    return markStyle;
+  };
+
+  const appendWatermark = (base64Url: string, markWidth: number) => {
+    if (containerRef.value && watermarkRef.value) {
+      stopObservation.value = true;
+      watermarkRef.value.setAttribute(
+        'style',
+        getStyleStr({
+          ...getMarkStyle(),
+          backgroundImage: `url('${base64Url}')`,
+          backgroundSize: `${Math.floor(markWidth)}px`,
+        }),
+      );
+      containerRef.value?.append(watermarkRef.value);
+      // Delayed execution
+      setTimeout(() => {
+        stopObservation.value = false;
+      });
+    }
+  };
+
+  const reRendering = (
+    mutation: MutationRecord,
+    watermarkElement?: HTMLElement,
+  ) => {
+    let flag = false;
+    // delete
+    if (mutation.removedNodes.length > 0 && watermarkElement) {
+      flag = Array.from(mutation.removedNodes).includes(watermarkElement);
+    }
+    // property value has been modified
+    if (mutation.type === 'attributes' && mutation.target === watermarkElement) {
+      flag = true;
+    }
+    return flag;
+  };
+
+  const destroyWatermark = () => {
+    if (watermarkRef.value) {
+      watermarkRef.value.remove();
+      watermarkRef.value = undefined;
+    }
+  };
+
+  const getMarkSize = (ctx: CanvasRenderingContext2D) => {
+    let defaultWidth = 120;
+    let defaultHeight = 64;
+    let space = 0;
+
+    if (!image.value && ctx.measureText) {
+      ctx.font = `${Number(fontOptions.value.fontSize)}px ${fontOptions.value.fontFamily}`;
+
+      const contents = isArray(content.value) ? content.value : [content.value];
+      let maxWidth = 0;
+      let maxHeight = 0;
+      for (const item of contents) {
+        const {
+          width,
+          fontBoundingBoxAscent,
+          fontBoundingBoxDescent,
+          actualBoundingBoxAscent,
+          actualBoundingBoxDescent,
+        } = ctx.measureText(item!);
+        // Using `actualBoundingBoxAscent` to be compatible with lower version browsers (eg: Firefox < 116)
+        const height = isUndefined(fontBoundingBoxAscent)
+          ? actualBoundingBoxAscent + actualBoundingBoxDescent
+          : fontBoundingBoxAscent + fontBoundingBoxDescent;
+
+        if (width > maxWidth) maxWidth = Math.ceil(width);
+        if (height > maxHeight) maxHeight = Math.ceil(height);
+      }
+
+      defaultWidth = maxWidth;
+      defaultHeight = maxHeight * contents.length + (contents.length - 1) * fontOptions.value.fontGap;
+      console.log();
+
+      const angle = (Math.PI / 180) * Number(rotate.value);
+      space = Math.ceil(Math.abs(Math.sin(angle) * defaultHeight) / 2);
+
+      defaultWidth += space;
+    }
+
+    return [defaultWidth, defaultHeight, space] as const;
+  };
+
+  const renderWatermark = () => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    if (ctx) {
+      if (!watermarkRef.value) {
+        watermarkRef.value = document.createElement('div');
+      }
+
+      const ratio = getPixelRatio();
+      const [markWidth, markHeight, space] = getMarkSize(ctx);
+
+      const drawCanvas = (
+        drawContent?: UnwrapRef<NonNullable<WatermarkOptions['content']>> | HTMLImageElement,
+      ) => {
+        const [textClips, clipWidth] = getClips(
+          drawContent || '',
+          rotate.value,
+          ratio,
+          markWidth,
+          markHeight,
+          fontOptions.value,
+          gapX.value,
+          gapY.value,
+          space,
+        );
+        appendWatermark(textClips, clipWidth);
+      };
+
+      if (image?.value) {
+        const img = new Image();
+        img.addEventListener('load', () => {
+          drawCanvas(img);
+        });
+        img.addEventListener('error', () => {
+          drawCanvas(content.value);
+        });
+        img.crossOrigin = 'anonymous';
+        img.referrerPolicy = 'no-referrer';
+        img.src = image.value;
+      }
+      else {
+        drawCanvas(content.value);
+      }
+    }
+  };
+
+  const onMutate = (mutations: MutationRecord[]) => {
+    if (stopObservation.value) {
+      return;
+    }
+    for (const mutation of mutations) {
+      if (reRendering(mutation, watermarkRef.value)) {
+        destroyWatermark();
+        renderWatermark();
+      }
+    }
+  };
+
+  useMutationObserver(containerRef, onMutate, {
+    attributes: true,
+    subtree: true,
+    childList: true,
+  });
+
+  onMounted(() => {
+    if (el.value) {
+      containerRef.value = document.createElement('div');
+      containerRef.value.style.position = 'relative';
+      const parent = el.value.parentNode;
+      parent?.insertBefore(containerRef.value, el.value);
+      containerRef.value.appendChild(el.value);
+    }
+    renderWatermark();
+  });
+  watch(
+    () => [options, el.value],
+    renderWatermark,
+    {
+      deep: true,
+      flush: 'post',
+    },
+  );
+};
--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,14 +1,57 @@
 <script setup lang="ts">
 import { useDark, useToggle } from '@vueuse/core';
+import { useWatermark } from 'cdx-component';
+import { reactive, ref, watch } from 'vue';
 
 const isDark = useDark();
 const toggleDark = useToggle(isDark);
+
+const containerRef = ref<HTMLElement>();
+const content = ref(['cdx-component', '12345']);
+const rotate = ref(-22);
+const font = reactive({
+  fontSize: 14,
+  color: '#333',
+});
+const gap = ref([20, 20]);
+useWatermark(containerRef, {
+  content,
+  rotate,
+  font,
+  gap,
+});
+
+function changeWatermark() {
+  content.value[1] = Math.random().toString(36).slice(2);
+  rotate.value += 20;
+  rotate.value %= 360;
+  font.fontSize += 1;
+}
+
+watch(
+  isDark,
+  () => {
+    font.color = isDark.value
+      ? 'rgba(255, 255, 255, .15)'
+      : 'rgba(0, 0, 0, .15)';
+  },
+  {
+    immediate: true,
+  },
+);
 </script>
 
 <template>
   <div>
-    <button class="border-lg fixed bottom-6 right-6 rounded-lg border p-2" @click="toggleDark()">
+    <div ref="containerRef" class="h-screen" @click="changeWatermark" />
+    <button class="border-lg fixed bottom-6 right-6 rounded-lg border p-2 text-slate-500 dark:text-slate-400" @click="toggleDark()">
       {{ isDark ? 'dark' : 'light' }}
     </button>
   </div>
 </template>
+
+<style>
+body {
+  background-color: var(--cdx-bg-color-base);
+}
+</style>
